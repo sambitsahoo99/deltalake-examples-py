d38819d0ba6aadd8db2ca180534ede434896dbe4
SidharthaRay <sidhartha.ray533@gmail.com>
2020-11-16 21:07:10 +0530

    inital commit

--- com/dsm/delta/merge_demo.py
+++ com/dsm/delta/merge_demo.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
             .whenNotMatchedInsert(values={"country": "updates.country", "year": "updates.year", "temperature": "updates.temperature"}) \
             .execute()
 
-        delta_merge_df.toDF.show()
+        delta_merge_df.toDF().show()
 
 
 # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4,io.delta:delta-core_2.11:0.6.0" com/dsm/delta/merge_demo.py
